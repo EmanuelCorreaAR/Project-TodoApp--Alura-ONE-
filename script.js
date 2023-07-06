3b1ab82e876f0f6cd4b7b14bae8d200aae124a28
EmanuelCorreaAR <correa.emanuel@outlook.com>
2023-07-06 12:40:12 -0300

    createTask

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('[data-form-btn]')
+
+const createTask = (event)  => {
+    event.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+}
+
+btn.addEventListener('click', createTask )
